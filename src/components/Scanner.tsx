38362b35c808ce76e294736edb53e6c90ebb0987
itswil <ww8668@gmail.com>
2022-11-30 19:54:35 +0800

    Update layouts

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -23,17 +23,7 @@ export const Scanner = () => {
         className="aspect-square object-cover"
         style={{ clipPath: "url(#squircle)" }}
       />
-      <pre>{JSON.stringify(result, null, 2)}</pre>
-      <svg>
-        <defs>
-          <clipPath id="squircle" clipPathUnits="objectBoundingBox">
-            <path
-              d="M .5 0 C .1 0 0 .1 0 .5 0 .9 .1 1 .5 1 .9 1 1 .9 1 .5 1 .1 .9 0 .5 0 Z"
-              fill="#f00"
-            />
-          </clipPath>
-        </defs>
-      </svg>
+      {/* <pre>{JSON.stringify(result, null, 2)}</pre> */}
     </>
   );
 };
